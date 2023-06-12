ee797c8bfca194528d0d1e912e38282bea9bea93
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-06-12 12:41:56 -0300

    feat: Finalizando a integração do tema com o wordpress

--- home.php
+++ home.php
@@ -287,7 +287,7 @@ $theme = get_bloginfo("template_url");
             } else { ?>
               <div id="detalhe-<?= get_the_ID(); ?>" class="container-fluid d-none" data-detalhe>
                 <div class="row px-awe-32">
-                  <div class="col-12 col-lg-5 order-first order-lg-last">
+                  <div class="col-12 order-first order-lg-last">
                     <div class="d-block pb-awe-16">
                       <h3 class="emphasis-2 p-1 d-inline text-black-2 fz-24 ff-ubuntu fw-regular">
                         Linha
@@ -439,6 +439,57 @@ $theme = get_bloginfo("template_url");
   </div>
 </section>
 
+<section id="duvidas">
+  <div class="duvidas-normal">
+    <div class="container py-awe-108 py-lg-awe-64">
+      <div class="row">
+        <div class="col-12 col-lg-1 d-flex justify-content-center justify-content-lg-start pb-awe-32 pb-lg-0 position-relative gotas">
+          <div class="pt-awe-6">
+            <img width="60" src="<?= $theme; ?>/dist/img/svg/whatsapp.svg" alt="">
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="pb-awe-16">
+            <h5 class="ff-ubuntu fz-32 text-white text-center text-lg-start">
+              Ficou com dúvida ou podemos te ajudar de outra forma?
+            </h5>
+          </div>
+          <div class="pt-awe-16 pb-awe-64 pb-lg-0 pt-lg-0">
+            <p class="ff-open-sans fz-18 text-white text-center text-lg-start">
+              Temos consultores preparados para te ajudar no que você precisar. Entre em contato e descubra como podemos
+              te surpreender.
+            </p>
+          </div>
+        </div>
+        <div class="col-12 col-lg-5 d-flex justify-content-center justify-content-lg-end align-items-center">
+          <a href="<?= get_field('whatsapp_link', get_option('home_page_id')); ?>" class="btn btn-blue-2 text-white text-uppercase fz-16 fw-semi-bold ff-open-sans">
+            fale com nosso atendimento
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="duvidas-fixo">
+    <div class="container py-awe-16">
+      <div class="row">
+        <div class="col-1">
+          <img width="60" src="<?= $theme; ?>/dist/img/svg/whatsapp.svg" alt="">
+        </div>
+        <div class="col-3 px-0 d-flex align-items-center">
+          <h5 class="ff-open-sans fz-18 text-white">
+            Ficou com dúvida ou podemos te ajudar de outra forma?
+          </h5>
+        </div>
+        <div class="col-8 d-flex justify-content-end align-items-center">
+          <a href="<?= get_field('whatsapp_link', get_option('home_page_id')); ?>" class="btn btn-blue-2 text-white text-uppercase fz-16 fw-semi-bold ff-open-sans">
+            fale com nosso atendimento
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <section id="sobre" class="container pt-awe-96 pb-awe-150">
   <div class="row justify-content-between">
     <div class="col-12">
@@ -459,6 +510,88 @@ $theme = get_bloginfo("template_url");
     <div class="col-12 col-lg-6 py-awe-32 py-lg-0">
       <div class="fz-18 ff-open-sans text-black-3 sobre-nos">
         <?= get_field('sobre_nos', get_option('home_page_id')); ?>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="orcamento">
+  <div class="container">
+    <div class="row justify-content-center justify-content-lg-start">
+      <div class="col-12 col-lg-4 text-black-4" id="#contatos">
+        <h4 class="fz-24 fw-bold ff-ubuntu">
+          Nossos contatos
+        </h4>
+        <div class="pt-awe-64 d-flex flex-lg-column flex-wrap gap-awe-32">
+          <div>
+            <h5 class="ff-open-sans fz-16 fw-semi-bold">
+              Endereço
+            </h5>
+            <p class="ff-open-sans fz-14">
+              <?= get_field('endereco_empresa', get_option('home_page_id')); ?> <br> CEP:
+              <?= get_field('cep_empresa', get_option('home_page_id')); ?>
+            </p>
+          </div>
+          <div>
+            <h5 class="ff-open-sans fz-16 fw-semi-bold">
+              Fale pelo telefone
+            </h5>
+            <p class="ff-open-sans fz-14">
+            <p class="text-black-3 text-orange-hover text-decoration-none d-inline">
+              <?= get_field('telefone_empresa_1', get_option('home_page_id')); ?>
+            </p> |
+            <p class="text-black-3 text-orange-hover text-decoration-none d-inline">
+              <?= get_field('telefone_empresa_2', get_option('home_page_id')); ?>
+            </p> <br>
+            <a href="mailto:<?= get_field('email_atendimento', get_option('home_page_id')); ?>" class="text-black-3 text-orange-hover text-decoration-none">
+              <?= get_field('email_atendimento', get_option('home_page_id')); ?>
+            </a>
+            </p>
+          </div>
+          <div>
+            <h5 class="ff-open-sans fz-16 fw-semi-bold">
+              Nossas mídias
+            </h5>
+            <span class="d-inline-flex gap-3">
+              <a target="_blank" href="<?= get_field('instagram_link', get_option('home_page_id')); ?>" class="">
+                <img src="<?= $theme; ?>/dist/img/svg/instagram-dark.svg" alt="">
+              </a>
+              <a target="_blank" href="<?= get_field('whatsapp_link', get_option('home_page_id')); ?>" class="">
+                <img src="<?= $theme; ?>/dist/img/svg/whatsapp-dark.svg" alt="">
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-10 col-lg-6 offset-lg-2 order-first order-lg-last px-lg-awe-32 pb-awe-64 pb-lg-0">
+        <?= do_shortcode('[contact-form-7 id="5" title="Formulário de orçamento"]'); ?>
+        <!-- <div class="ff-open-sans">
+          <h3 class="text-blue ff-ubuntu fz-32 text-center">
+            Solicite um orçamento
+          </h3>
+          <p class="text-center text-black-3 fz-18">
+            Preencha os campos abaixo para realizarmos um orçamento com o melhor custo-benefício do mercado.
+          </p>
+          <div class="d-flex flex-column gap-awe-22 fz-14 text-black-2 pt-awe-32 pb-awe-56">
+            <div>
+              <label>Nome</label>
+              <input type="text" placeholder="Digite seu nome">
+            </div>
+            <div>
+              <label>Telefone</label>
+              <input type="number" placeholder="Digite um número de telefone">
+            </div>
+            <div>
+              <label>E-mail</label>
+              <input type="email" placeholder="Digite seu e-mail">
+            </div>
+            <div>
+              <label>Cidade/UF</label>
+              <input class="form-control" type="text" placeholder="Digite a cidade e estado de instalação da piscina">
+            </div>
+          </div>
+          <button type="submit">Solicitar atendimento</button>
+        </div> -->
       </div>
     </div>
   </div>
